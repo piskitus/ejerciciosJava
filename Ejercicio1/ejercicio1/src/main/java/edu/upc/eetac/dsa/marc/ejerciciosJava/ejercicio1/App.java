6598aa10701e1d833cb4238e834f6d3829fdcb91
piskitus <marcalarcon1994@gmail.com>
2015-02-25 18:08:19 +0100

    ej1

---
+++ Ejercicio1/ejercicio1/src/main/java/edu/upc/eetac/dsa/marc/ejerciciosJava/ejercicio1/App.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa.marc.ejerciciosJava.ejercicio1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
